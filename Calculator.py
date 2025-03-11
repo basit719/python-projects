77f3b4c01531da8cb766e890158a013d0c608750
basit719 <basit.r@turing.com>
2025-03-11 23:22:15 +0500

    Multiply and divide functionalities

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,14 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -31,6 +39,12 @@ while True:
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+        
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
             
         # check if user wants another calculation
         # break the while loop if answer is no
